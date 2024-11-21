4a989c01ac5a663428763ab172a9d528fd05e7d9
Edgar Lopez <edgarlopez755@gmail.com>
2024-11-21 12:33:22 -0800

    Define initial state

--- src/components/Bookshelf.jsx
+++ src/components/Bookshelf.jsx
@@ -1,2 +1,11 @@
 import { useState } from 'react';
 
+const Bookshelf = () => {
+    const [books, setBook] = useState([])
+    const [newBook, setNewBook] = useState({
+        title: '',
+        author: ''
+    })
+
+    
+}
